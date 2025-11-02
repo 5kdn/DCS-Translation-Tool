71f45a20827c7c07e4c177d1b446c9f1dd4d1043
5kdn <sakuden542+github@gmail.com>
2025-11-03 02:52:08 +0900

    feat: fix version (#24)
    * feat: Manage the app version with release-please
    
    * fix: versionの取得をInfra層からPresentation層へ修正

---
+++ tests/DcsTranslationTool.Presentation.Wpf.Tests/Services/ApplicationInfoServiceTests.cs
@@ -0,0 +1,25 @@
+using DcsTranslationTool.Presentation.Wpf.Services;
+
+using Moq;
+
+using Xunit;
+
+namespace DcsTranslationTool.Presentation.Wpf.Tests.Services;
+
+public sealed class ApplicationInfoServiceTests {
+    private readonly Mock<ILoggingService> logger = new();
+
+    [Fact]
+    public void GetVersionは実行アセンブリにバージョン情報が含まれているときに正しいバージョンを返す() {
+        // Arrange
+        var sut = new ApplicationInfoService( logger.Object );
+
+        // Act
+        var version = sut.GetVersion();
+
+        // Assert
+        Assert.NotNull( version );
+        Assert.True( version.Major >= 0 );
+        Assert.True( version.Minor >= 0 );
+    }
+}
