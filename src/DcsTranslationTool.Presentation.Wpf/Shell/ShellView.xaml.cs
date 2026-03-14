845c14022ef3ac152d59f34b5fd819fac70d2c18
5kdn <sakuden542+github@gmail.com>
2026-03-14 21:46:51 +0900

    fix(shell): Shell の最小サイズをコンテンツの最小要求で再計算する

--- src/DcsTranslationTool.Presentation.Wpf/Shell/ShellView.xaml.cs
+++ src/DcsTranslationTool.Presentation.Wpf/Shell/ShellView.xaml.cs
@@ -28,13 +28,34 @@ public partial class ShellView : Window {
     /// 現在のレイアウト要求に基づいて Shell の最小サイズを更新する。
     /// </summary>
     private void UpdateMinimumShellSize() {
-        RootLayout.UpdateLayout();
-
-        var desiredSize = RootLayout.DesiredSize;
+        var desiredSize = MeasureMinimumContentSize( RootLayout );
         var windowChrome = WindowChrome.GetWindowChrome( this );
         var resizeBorderThickness = windowChrome?.ResizeBorderThickness ?? default;
 
         MinWidth = desiredSize.Width + resizeBorderThickness.Left + resizeBorderThickness.Right;
         MinHeight = desiredSize.Height + resizeBorderThickness.Top + resizeBorderThickness.Bottom;
+    }
+
+    /// <summary>
+    /// 要素が必要とする最小コンテンツサイズを計測する。
+    /// </summary>
+    /// <param name="element">計測対象の要素。</param>
+    /// <returns>無制約計測で得られた最小要求サイズ。</returns>
+    internal static Size MeasureMinimumContentSize( FrameworkElement element ) {
+        ArgumentNullException.ThrowIfNull( element );
+
+        var restoreSize = new Size(
+            Math.Max( element.ActualWidth, 0 ),
+            Math.Max( element.ActualHeight, 0 ) );
+
+        element.UpdateLayout();
+        element.Measure( new Size( double.PositiveInfinity, double.PositiveInfinity ) );
+        var desiredSize = element.DesiredSize;
+
+        if(restoreSize.Width > 0 || restoreSize.Height > 0) {
+            element.Measure( restoreSize );
+        }
+
+        return desiredSize;
     }
 }
